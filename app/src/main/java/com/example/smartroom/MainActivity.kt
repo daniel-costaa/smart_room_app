608e2fd3229f3ec15e88bad10953eb6828fbcd18
Daniel Costa <36517112+daniel-costaa@users.noreply.github.com>
2021-11-09 19:47:53 -0300

    Initial commit

--- app/src/main/java/com/example/smartroom/MainActivity.kt
+++ app/src/main/java/com/example/smartroom/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentOne = FragmentOne()
+        supportFragmentManager.beginTransaction().replace(R.id.flFrament, fragmentOne)
     }
 }
