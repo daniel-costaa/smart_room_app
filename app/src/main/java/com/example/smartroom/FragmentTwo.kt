aa17b6c85b3a4ef523313e4706d9d19943f3aed5
Daniel Costa <36517112+daniel-costaa@users.noreply.github.com>
2021-11-17 19:36:55 -0300

    Fixed fragments

--- app/src/main/java/com/example/smartroom/FragmentTwo.kt
+++ app/src/main/java/com/example/smartroom/FragmentTwo.kt
@@ -3,13 +3,13 @@ package com.example.smartroom
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.example.smartroom.databinding.FragmentOneBinding
+import com.example.smartroom.databinding.FragmentTwoBinding
 
+class FragmentTwo : Fragment(R.layout.fragment_two) {
+    private lateinit var binding: FragmentTwoBinding
 
-class FragmentOne : Fragment(R.layout.fragment_one) {
-    private lateinit var binding: FragmentOneBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentOneBinding.bind(view)
+        binding = FragmentTwoBinding.bind(view)
     }
 }
