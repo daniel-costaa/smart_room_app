88ab6368d9e8fa80414f769ae3fc1900a9f4f535
Daniel Costa <36517112+daniel-costaa@users.noreply.github.com>
2021-11-23 19:47:07 -0300

    Changed ResultClass

--- app/src/main/java/com/example/smartroom/common/Result.kt
+++ app/src/main/java/com/example/smartroom/common/Result.kt
@@ -1,10 +1,9 @@
 package com.example.smartroom.common
 
 sealed class Result<out T> {
-    object Loading : Result<Nothing>()
-    data class Success<T>(val data: T) : Result<T>()
-    data class Error(
-        val t: Throwable,
-        var consumed: Boolean = false
-    ) : Result<Nothing>()
+    data class Success<out R>(val value: R): Result<R>()
+    data class Failure(
+        val message: String?,
+        val throwable: Throwable?
+    ): Result<Nothing>()
 }
