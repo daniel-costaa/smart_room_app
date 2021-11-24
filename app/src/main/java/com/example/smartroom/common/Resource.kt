106bb29af67f00be6a1742e63828307a71f0113c
Daniel Costa <36517112+daniel-costaa@users.noreply.github.com>
2021-11-24 19:27:03 -0300

    Added dependencies and changed Resource class

--- app/src/main/java/com/example/smartroom/common/Resource.kt
+++ app/src/main/java/com/example/smartroom/common/Resource.kt
@@ -1,40 +1,13 @@
 package com.example.smartroom.common
 
-import androidx.annotation.NonNull
-import androidx.annotation.Nullable
-
-class Resource<T> private constructor(@NonNull val status: Status,
-                                      @Nullable val data: T?,
-                                      @Nullable val message: String?) {
-
-    enum class Status {
-        SUCCESS, ERROR, LOADING
-    }
+sealed class Resource<T> {
+    class Loading<T> : Resource<T>()
+    data class Success<T>(val data: T) : Resource<T>()
+    data class Failed<T>(val message: String) : Resource<T>()
 
     companion object {
-
-        fun <T> success(@NonNull data: T): Resource<T> {
-            return Resource(
-                Status.SUCCESS,
-                data,
-                null
-            )
-        }
-
-        fun <T> error(msg: String, @Nullable data: T?): Resource<T> {
-            return Resource(
-                Status.ERROR,
-                data,
-                msg
-            )
-        }
-
-        fun <T> loading(@Nullable data: T? = null): Resource<T> {
-            return Resource(
-                Status.LOADING,
-                data,
-                null
-            )
-        }
+        fun <T> loading() = Loading<T>()
+        fun <T> success(data: T) = Success(data)
+        fun <T> failed(message: String) = Failed<T>(message)
     }
 }
