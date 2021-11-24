fe2ffdaf21c99fc709d87d4f41bb490df656e5a1
Daniel Costa <36517112+daniel-costaa@users.noreply.github.com>
2021-11-24 20:45:47 -0300

    Improved firebase repository

--- app/src/main/java/com/example/smartroom/network/FirebaseRepositoryImpl.kt
+++ app/src/main/java/com/example/smartroom/network/FirebaseRepositoryImpl.kt
@@ -1,7 +1,40 @@
 package com.example.smartroom.network
 
+import com.example.smartroom.common.Resource
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
 class FirebaseRepositoryImpl : FirebaseRepository {
-    override fun getAllSensorData() {
+
+
+    private val firebaseDatabaseReference: FirebaseDatabase by lazy {
+        Firebase.database
     }
 
+    @ExperimentalCoroutinesApi
+    override suspend fun getAllSensorData(): Flow<Resource<Double>> = callbackFlow {
+
+        val listener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val data = snapshot.value as Double
+                this@callbackFlow.trySendBlocking(Resource.success(data))
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                this@callbackFlow.trySendBlocking(Resource.failed(error.message))
+            }
+        }
+        firebaseDatabaseReference.getReference("temperatura").addValueEventListener(listener)
+
+        awaitClose { firebaseDatabaseReference.getReference("temperatura").removeEventListener(listener) }
+    }
 }
