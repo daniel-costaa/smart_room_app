f9597392fba7e277f8fdc0cb5e3ebb37b1d8a989
Daniel Costa <36517112+daniel-costaa@users.noreply.github.com>
2021-11-25 21:24:38 -0300

    Improved firebase listener

--- app/src/main/java/com/example/smartroom/network/FirebaseRepositoryImpl.kt
+++ app/src/main/java/com/example/smartroom/network/FirebaseRepositoryImpl.kt
@@ -9,7 +9,6 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
@@ -23,11 +22,11 @@ class FirebaseRepositoryImpl : FirebaseRepository {
         val listener = object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val data = snapshot.value as Double
-                this@callbackFlow.trySendBlocking(Resource.success(data))
+                trySend(Resource.success(data))
             }
 
             override fun onCancelled(error: DatabaseError) {
-                this@callbackFlow.trySendBlocking(Resource.failed(error.message))
+                trySend(Resource.failed(error.message))
             }
         }
         firebaseDatabaseReference.getReference("temperatura").addValueEventListener(listener)
